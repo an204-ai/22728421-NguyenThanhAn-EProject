dd328dbb2ae271242e713d929511d1abee87b7b8
Thanh An <thanhan8912@gmail.com>
2025-10-30 17:36:08 +0700

    ci-cd

--- product/src/config.js
+++ product/src/config.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 
 module.exports = {
   port: process.env.PORT || 3001,
-  mongoURI: process.env.MONGODB_PRODUCT_URI || "mongodb://localdockerhost/products",
-  rabbitMQURI: process.env.RABBITMQ_URI || "amqp://localhost",
+  mongoURI: process.env.MONGODB_PRODUCT_URI || "mongodb://mongo:27017/product",
+  rabbitMQURI: process.env.RABBITMQ_URI || "amqp://guest:guest@rabbitmq:5672",
   exchangeName: "products",
   queueName: "products_queue",
 };
