d60277429be60859e3f7b1eb8a144317074c0468
Thanh An <thanhan8912@gmail.com>
2025-10-31 15:01:19 +0700

    update

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,7 +110,7 @@ class ProductController {
     }
   }
 
-  async getid(req, res){
+  async getid (req, res){
     const pd = await Product.findById(req.params.id);
     res.status(200).json(pd);
   }
